2483f441549a5417c698ea417f75a3ed0fc28b28
Iliass Hilmi <inf2766@hs-worms.de>
2020-09-08 15:15:35 +0200

    Add hello world app files

---
+++ index.js
@@ -0,0 +1,16 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response) {
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
+
+});
+
+console.log("hallo1");
+console.log("hallo2");
+console.log("hallo3");
+var port = 80;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
