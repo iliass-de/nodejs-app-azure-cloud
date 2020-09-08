5ed5e4692af0c976402de0797fbc53f1604f77ed
Iliass Hilmi <inf2766@hs-worms.de>
2020-09-08 17:11:11 +0200

    Change text content

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("<h1>Hello from Iliass</h1>");
+    response.end("Hello from Iliass");
 
 });
 
