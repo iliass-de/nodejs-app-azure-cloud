305f21c579975b2665dc46b2fbeadcbc2735690e
Iliass Hilmi <inf2766@hs-worms.de>
2020-09-08 16:12:14 +0200

    Change port from 80 to 8080

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var server = http.createServer(function(request, response) {
 console.log("hallo1");
 console.log("hallo2");
 console.log("hallo3");
-var port = 80;
+var port = 8080;
 server.listen(port);
 
 console.log("Server running at http://localhost:%d", port);
